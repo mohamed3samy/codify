20866642dad76d61fdc869d37eb0faf40fa7fb96
Mohamed Samy <52619427+mohamed3samy@users.noreply.github.com>
2024-02-03 11:09:35 +0200

    Update MobileNav.tsx

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -83,7 +83,7 @@ const MobileNav = () => {
 					/>
 
 					<p className="h2-bold text-dark100_light900 font-spaceGrotesk">
-						Dev<span className="text-primary-500">Flow</span>
+						Cod<span className="text-primary-500">ify</span>
 					</p>
 				</Link>
 				<div>
