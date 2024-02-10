b3d816297678f2144d78a4d04fe1fb0d18175d15
mohamed3samy <mohamed701samy@gmail.com>
2024-02-11 00:23:08 +0200

    hide scrollbar from nav in mobile

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -17,7 +17,7 @@ const NavContent = () => {
 	const pathname = usePathname();
 
 	return (
-		<section className="flex h-full flex-col gap-6 pt-16">
+		<section className="flex h-full flex-col gap-6 pt-16 sideBar">
 			{sidebarLinks.map((item) => {
 				const isActive =
 					(pathname.includes(item.route) &&
