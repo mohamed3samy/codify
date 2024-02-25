9c56933c064791cfc7a20843a4500829d64163dc
Mohamed Samy <mohamed701samy@gmail.com>
2024-02-25 19:37:20 +0200

    add some styles to navbar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -7,44 +7,44 @@ import MobileNav from './MobileNav';
 import GlobalSearch from '../search/GlobalSearch';
 
 const Navbar = () => {
-  return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center gap-1">
-        <Image
-          src="/assets/images/site-logo.svg"
-          width={23}
-          height={23}
-          alt="Codify"
-        />
+	return (
+		<nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+			<Link href="/" className="flex items-center gap-1">
+				<Image
+					src="/assets/images/site-logo.svg"
+					width={23}
+					height={23}
+					alt="Codify"
+				/>
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden tracking-wider lg:tracking-widest uppercase">
-          Cod<span className="text-primary-500">ifÿ</span>
-        </p>
-      </Link>
+				<p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden tracking-wider lg:tracking-widest uppercase">
+					Cod<span className="text-primary-500">ifÿ</span>
+				</p>
+			</Link>
 
-      <GlobalSearch />
+			<GlobalSearch />
 
-      <div className="flex-between gap-0 md:gap-5 sidebar">
-        <Theme />
+			<div className="flex-between gap-0 md:gap-5">
+				<Theme />
 
-        <SignedIn>
-          <UserButton
-            afterSignOutUrl="/"
-            appearance={{
-              elements: {
-                avatarBox: 'h-10 w-10',
-              },
-              variables: {
-                colorPrimary: '#ff7000',
-              },
-            }}
-          />
-        </SignedIn>
+				<SignedIn>
+					<UserButton
+						afterSignOutUrl="/"
+						appearance={{
+							elements: {
+								avatarBox: 'h-10 w-10',
+							},
+							variables: {
+								colorPrimary: '#ff7000',
+							},
+						}}
+					/>
+				</SignedIn>
 
-        <MobileNav />
-      </div>
-    </nav>
-  );
+				<MobileNav />
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
